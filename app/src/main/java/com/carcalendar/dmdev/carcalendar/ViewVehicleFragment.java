108c2902ed2e1819d01fa11e5befce580eca34aa
DraganovMartin <draganovmartin@abv.bg>
2016-12-22 15:39:58 +0200

    Added fragment for displaying vehicle data and layout for car.

---
+++ app/src/main/java/com/carcalendar/dmdev/carcalendar/ViewVehicleFragment.java
@@ -0,0 +1,29 @@
+package com.carcalendar.dmdev.carcalendar;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ViewVehicleFragment extends Fragment {
+
+
+    public ViewVehicleFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_view_car, container, false);
+    }
+
+}
