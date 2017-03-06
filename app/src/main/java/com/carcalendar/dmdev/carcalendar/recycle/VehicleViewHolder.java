5cd634c5d2e477c82cd85f9e12052436e6c54b90
DraganovMartin <draganovmartin@abv.bg>
2017-03-06 22:29:19 +0200

    Broken commit 2

--- app/src/main/java/com/carcalendar/dmdev/carcalendar/recycle/VehicleViewHolder.java
+++ app/src/main/java/com/carcalendar/dmdev/carcalendar/recycle/VehicleViewHolder.java
@@ -7,41 +7,20 @@ import android.widget.TextView;
 
 import com.carcalendar.dmdev.carcalendar.R;
 
-public class VehicleViewHolder extends RecyclerView.ViewHolder implements View.OnLongClickListener {
+public class VehicleViewHolder extends RecyclerView.ViewHolder {
 
     protected ImageView vehicleImage;
     protected TextView vehicleBrand;
     protected TextView vehicleModel;
     protected TextView vehicleYear;
     protected TextView vehicleRange;
-    private OnRecyclerViewItemLongPressListener recyclerViewLongPressListener;
 
-    public VehicleViewHolder(View view,OnRecyclerViewItemLongPressListener rViewLongPressListener){
+    public VehicleViewHolder(View view){
         super(view);
         vehicleImage = (ImageView) view.findViewById(R.id.vehicle_image);
         vehicleBrand = (TextView) view.findViewById(R.id.vehicle_brand);
         vehicleModel = (TextView) view.findViewById(R.id.vehicle_model_view_TV);
         vehicleYear = (TextView) view.findViewById(R.id.year_vehicle_view_TV);
         vehicleRange = (TextView) view.findViewById(R.id.range_vehicle_view_TV);
-        view.setOnLongClickListener(this);
-        recyclerViewLongPressListener = rViewLongPressListener;
-    }
-
-    @Override
-    public boolean onLongClick(View v) {
-        recyclerViewLongPressListener.onRecyclerViewItemLongPress(v,getAdapterPosition());
-        return true;
-    }
-
-    /**
-     * This interface provides the means for handling long click events on recycler view items
-     */
-    public interface OnRecyclerViewItemLongPressListener{
-        /**
-         *
-         * @param v the view on which the user long pressed
-         * @param pos the position of <code>v</code> in the recycler view
-         */
-        void onRecyclerViewItemLongPress(View v,final int pos);
     }
 }
