13416ffac1e419922cb8c433b1f568ab057fb648
dimchoSCR <dimchokarpachev@gmail.com>
2017-04-01 14:05:02 +0300

    Layout improvements

--- app/src/main/java/com/carcalendar/dmdev/carcalendar/AddVehicleMotorcycleActivity.java
+++ app/src/main/java/com/carcalendar/dmdev/carcalendar/AddVehicleMotorcycleActivity.java
@@ -1,9 +1,13 @@
 package com.carcalendar.dmdev.carcalendar;
 
+import android.app.DatePickerDialog;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class AddVehicleMotorcycleActivity extends AppCompatActivity {
+import com.carcalendar.dmdev.carcalendar.dialogs.DatePickerFragment;
+
+public class AddVehicleMotorcycleActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
